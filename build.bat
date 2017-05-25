35c036016c82279416855b16b23cc9a9870a82e5
Ryan Tomac <ryan@tomacfamily.com>
2017-05-25 20:25:35 +0000

    Run dotnet restore within each project dir

--- build.bat
+++ build.bat
@@ -1,6 +1,17 @@
 @echo off
 
+pushd "%~dp0Server\"
 dotnet restore
+popd
+
+pushd "%~dp0Client\"
+dotnet restore
+popd
+
+pushd "%~dp0ServerTests\"
+dotnet restore
+popd
+
 dotnet build
 
 pushd "%~dp0ServerTests\"
