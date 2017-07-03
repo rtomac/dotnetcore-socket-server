125e8402cd143264e899dc5b1c3cbbe753e89a9a
Ryan Tomac <ryan@tomacfamily.com>
2017-07-03 16:58:29 +0000

    Fix synchronization issues in status reporter

--- Server/StatusReporter.cs
+++ Server/StatusReporter.cs
@@ -12,28 +12,32 @@ namespace Server
     /// Creates a background thread to write status reports to the console
     /// on a specified interval.
     /// 
-    /// Uses reader/writer lock to synchronize access to the aggregate
-    /// numbers, since other threads processing data from connections will
-    /// be recording new numbers.
+    /// Uses <c>lock</c> to synchronize access to the aggregate
+    /// numbers, since [many] other threads processing data from
+    /// connections will be recording new numbers.
     /// 
     /// <c>_stopSignal</c> reset event used to release background thread
     /// and stop writing status reports when <c>Stop</c> is called.
     /// </remarks>
     public class StatusReporter
     {
-        public int TotalUnique { get; private set; }
-        public int TotalDuplicates { get; private set; }
-        public int IncrementalUnique { get; private set; }
-        public int IncrementalDuplicates { get; private set; }
+        public int TotalUnique => _totalUnique;
+        public int TotalDuplicates => _totalDuplicates;
+        public int IncrementalUnique => _incrementalUnique;
+        public int IncrementalDuplicates => _incrementalDuplicates;
 
-        private readonly ReaderWriterLockSlim _lock;
+        private int _totalUnique;
+        private int _totalDuplicates;
+        private int _incrementalUnique;
+        private int _incrementalDuplicates;
+        private readonly object _lock;
         private readonly ManualResetEventSlim _stopSignal;
 
         private static ILog _log = LogManager.GetLogger(typeof(StatusReporter));
 
         public StatusReporter()
         {
-            _lock = new ReaderWriterLockSlim();
+            _lock = new object();
             _stopSignal = new ManualResetEventSlim();
         }
 
@@ -62,29 +66,19 @@ namespace Server
 
         public void RecordUnique()
         {
-            _lock.EnterReadLock();
-            try
+            lock (_lock)
             {
-                IncrementalUnique += 1;
-                TotalUnique += 1;
-            }
-            finally
-            {
-                _lock.ExitReadLock();
+                _incrementalUnique++;
+                _totalUnique++;
             }
         }
 
         public void RecordDuplicate()
         {
-            _lock.EnterReadLock();
-            try
+            lock (_lock)
             {
-                IncrementalDuplicates += 1;
-                TotalDuplicates += 1;
-            }
-            finally
-            {
-                _lock.ExitReadLock();
+                _incrementalDuplicates++;
+                _totalDuplicates++;
             }
         }
 
@@ -104,18 +98,13 @@ namespace Server
             // Dereference the numbers we need, and write them afterword,
             // to minimize the amount of time we need to have a write lock
             // and be blocking other processing threads that are reporting status.
-            _lock.EnterWriteLock();
-            try
+            lock (_lock)
             {
-                incrementalUnique = IncrementalUnique;
-                incrementalDups = IncrementalDuplicates;
-                totalUnique = TotalUnique;
+                incrementalUnique = _incrementalUnique;
+                incrementalDups = _incrementalDuplicates;
+                totalUnique = _totalUnique;
 
-                IncrementalUnique = IncrementalDuplicates = 0;
-            }
-            finally
-            {
-                _lock.ExitWriteLock();
+                _incrementalUnique = _incrementalDuplicates = 0;
             }
 
             writer.Write($"Received {incrementalUnique} unique numbers, {incrementalDups} duplicates. Unique total: {totalUnique}");
